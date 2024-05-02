f798e4497128f51805dd27c44673a098efd86c13
David Szankovszky <szankovszky.david@gmail.com>
2024-05-02 13:08:43 +0200

    refactored, fly.toml modified

--- src/app/counter-handler/counter-handler.component.ts
+++ src/app/counter-handler/counter-handler.component.ts
@@ -1,81 +1,70 @@
-import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject, signal } from '@angular/core';
 import { CounterServiceService } from '../counterService/counter-service.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-counter-handler',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './counter-handler.component.html',
   styleUrl: './counter-handler.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterHandlerComponent implements OnInit {
-
-  constructor(
-    private counterService: CounterServiceService,
-  ) { }
+  counterService = inject(CounterServiceService)
 
   count = signal(0);
+  isAutoIncreasing = signal(false)
+  isAutoDecreasing = signal(false)
+  continousModificationId?: ReturnType<typeof setInterval>
 
-  getNumber() {
-    return this.counterService.getNumberFromServer();
+  async ngOnInit() {
+    await this.updateNumber()
   }
 
-  increaseNumber() {
-    this.counterService.increaseNumberOnServer()
-      .then(res => {
-        this.count.set(res)
-      })
+  async updateNumber(){
+    const number = await this.counterService.getNumberFromServer()
+    this.count.set(number)
   }
 
-  decreaseNumber() {
-    this.counterService.decreaseNumberOnServer()
-      .then(res => {
-        this.count.set(res)
-      })
+  async increaseNumber() {
+    await this.counterService.increaseNumberOnServer()
+    await this.updateNumber()
   }
 
-  @ViewChild('continuousIncreaseCheck') increaseCheckBox!: ElementRef;
-  @ViewChild('continuousDecreaseCheck') decreaseCheckBox!: ElementRef;
-
-  id: number | ReturnType<typeof setInterval> = 0;
-  continuousIncrease(e: Event) {
-    const isChecked = (<HTMLInputElement>e.target).checked;
-    if (isChecked) {
-      this.decreaseCheckBox.nativeElement.setAttribute("disabled", "")
-      this.id = setInterval(() => {
-        this.increaseNumber();
-      }, 1000)
-    }
-    else if (this.id != 0) {
-      this.decreaseCheckBox.nativeElement.removeAttribute("disabled")
-      console.log("Unchecked!")
-      clearInterval(this.id);
-      this.id = 0;
-    }
+  async decreaseNumber() {
+    await this.counterService.decreaseNumberOnServer()
+    await this.updateNumber()
   }
 
-  continuousDecrease(e: Event) {
-    const isChecked = (<HTMLInputElement>e.target).checked;
-    if (isChecked) {
-      this.increaseCheckBox.nativeElement.setAttribute("disabled", "")
-      console.log("Checked!")
-      this.id = setInterval(() => {
-        this.decreaseNumber();
-      }, 1000)
-    }
-    else if (this.id != 0) {
-      this.increaseCheckBox.nativeElement.removeAttribute("disabled")
-      console.log("Unchecked!")
-      clearInterval(this.id);
-      this.id = 0;
+  intervalModification() {
+    this.continousModificationId = setInterval(() => {
+      if(this.isAutoIncreasing()){
+        this.increaseNumber()
+      }
+      else if(this.isAutoDecreasing()){
+        this.decreaseNumber()
+      }
+    }, 1000)
+  }
+
+  continuousIncrease() {
+    this.clearIntervalId()
+    if (this.isAutoIncreasing()) {
+      this.intervalModification()
+      this.isAutoDecreasing.set(false)
     }
   }
 
+  continuousDecrease() {
+    this.clearIntervalId()
+    if (this.isAutoDecreasing()) {
+      this.intervalModification()
+      this.isAutoIncreasing.set(false)
+    }
+  }
 
-  ngOnInit(): void {
-    this.getNumber().then(res => {
-      this.count.set(res);
-    })
+  clearIntervalId() {
+    clearInterval(this.continousModificationId)
   }
 }
--- src/app/counterService/counter-service.service.ts
+++ src/app/counterService/counter-service.service.ts
@@ -1,50 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { createClient } from "@libsql/client";
+import { ResultSet, createClient } from "@libsql/client";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterServiceService {
 
-  constructor(private http: HttpClient) { }
-
   client = createClient({
     url: "libsql://counter-szankdav.turso.io",
     authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTA3NjE4MTgsImlkIjoiNWJhYmIwOTItODk0Mi00NTU1LWI3NDAtYWM3OTE0OWI1MTNjIn0.tuaOahYx0A4DoDyGkbXoRUg-YS8_flmeeYzU7vlSYMTO1KDT86jV6EzjEmozYtdui1mhBg-rXydKA5_zatmCCQ",
   });
 
-  getNumberFromServer(): Promise<number> {
-    return new Promise(async (res, rej) => {
-      try {
-        const result = await this.client.execute("SELECT countedNumber FROM countedNumbers");
-        const numberFromServer = result.rows[0]['countedNumber'];
-        res(Number(numberFromServer))
-      } catch (error) {
-        console.error("Error while fetching the data:\n", error)
-      }
-    })
+  async queryHandler(query: string): Promise<ResultSet | null>{
+    try {
+      return await this.client.execute(query);
+    } catch (error) {
+      console.error("Error while execute query: ", error)
+      return null
+    }
   }
 
-  increaseNumberOnServer(): Promise<number> {
-    return new Promise(async (res, rej) => {
-      try {
-        await this.client.execute("UPDATE countedNumbers SET countedNumber = countedNumber + 1");
-        res(this.getNumberFromServer())
-      } catch (error) {
-        console.error("Error while increasing the number:", error);
-      }
-    })
+   async getNumberFromServer(): Promise<number> {
+    const result = await this.queryHandler("SELECT countedNumber FROM countedNumbers");
+    return result?.rows[0]['countedNumber'] as number ?? 0;
   }
 
-  decreaseNumberOnServer(): Promise<number> {
-    return new Promise(async (res, rej) => {
-      try {
-        await this.client.execute("UPDATE countedNumbers SET countedNumber = countedNumber - 1");
-        res(this.getNumberFromServer())
-      } catch (error) {
-        console.error("Error while decreasing the number:", error);
-      }
-    })
+  async increaseNumberOnServer(): Promise<ResultSet | null> {
+    return await this.queryHandler("UPDATE countedNumbers SET countedNumber = countedNumber + 1")
+  }
+
+  async decreaseNumberOnServer(): Promise<ResultSet | null> {
+    return await this.queryHandler("UPDATE countedNumbers SET countedNumber = countedNumber - 1")
   }
 }
